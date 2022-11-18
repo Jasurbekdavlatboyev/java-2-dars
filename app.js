d11cdb4f0dca2904307231fa02b96b646c6b8b42
Jasurbekdavlatboyev <oddibola200@gmail.com>
2022-11-18 22:21:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+confirm("Odindan aytib utay siz ichki iwlar vazirligi sayytiga kiryapsz");
+let ism = prompt("Ismingiz");
+console.log("Ismingiz: " + ism);
+let familiya = prompt("Familiyangiz");
+console.log("Familiyangiz: " + familiya);
+let yosh = prompt("Yoshingiz");
+console.log("Yoshingiz: " + yosh);
+let karta = prompt("Sizga kirmang degandim nega kiryapsz");
+console.log("Kartangiz raqami: " + hop);
+let parol = prompt("endi telefon raqamizni yozing va 15 sutka uy qamogida bulasz");
+console.log("Parolingiz: " + hop);
+alert("endi siz ichki iwlar vazirligi tomonidan qamoq jazosiga mahkum etildingiz");
+alert("qamoqga marhamat");
+alert("Albatta buni hamasi hazir ")
